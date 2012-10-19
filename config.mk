336e6f8d1ede137bac6118c9e8cd995388682149
Paul Hoffman <nkuitse@nkuitse.com>
2012-10-19 12:36:07 -0400

    bump version, add metadata

--- config.mk
+++ config.mk
@@ -1,5 +1,8 @@
-PROG    = boj
-VERSION = 0.5.2
+PROG      = boj
+VERSION   = 0.6.0
+AUTHOR    = Paul Hoffman <paul@flo.org>
+COPYRIGHT = Copyright 2012 Fenway Libraries Online.
+
 PREFIX  = /usr/local
 BINDIR  = $(PREFIX)/bin
 MANDIR  = $(PREFIX)/share/man/man1
