e96147313209b80e17e670cf746226b134988e3f
Paul Hoffman <nkuitse@nkuitse.com>
2016-12-07 07:10:59 -0500

    bump version for release

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 PROG      = boj
-VERSION   = 0.6.9
+VERSION   = 1.00
 AUTHOR    = Paul Hoffman <paul@flo.org>
-COPYRIGHT = Copyright 2012-2014 Fenway Libraries Online.
+COPYRIGHT = Copyright 2012-2016 Fenway Libraries Online.
 
 PREFIX  = /usr/local
 BINDIR  = $(PREFIX)/bin
